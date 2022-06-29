1312a1ee1b5f32501be93efb82a310a2c499c216
Hadashm <hadashm@gmail.com>
2022-06-29 20:50:11 +0300

    setting sidebar

--- js/apps/mail/cmps/email-sidebar.cmp.js
+++ js/apps/mail/cmps/email-sidebar.cmp.js
@@ -1,5 +1,25 @@
 export default {
   template: `
+    <section class="email-sidebar-container">
+        <ul>
+            <li class="inbox">inbox
+                <img src="../../../img/mail_draft_black_20dp.png" alt="">
+            </li>
+            <li class="inbox">Sent
+                <img src="../../../img/mail_send_black_20dp.png" alt="">
+            </li>
+            <li class="inbox">Bin
+                <img src="../../../img/mail_delete_black_20dp.png" alt="">
+            </li>
+            <li class="inbox">Starred
+                <img src="../../../img/mail_star_black_20dp.png" alt="">
+            </li>
+            <li class="inbox">Draft
+                <img src="../../../img/mail_draft_black_20dp.png" alt="">
+            </li>
+        </ul>
+
+    </section>
     
     `,
 
--- js/apps/mail/pages/mail-app.js
+++ js/apps/mail/pages/mail-app.js
@@ -1,19 +1,20 @@
 import { emailService } from '../services/mail-services.js'
 import emailList from '../cmps/email-list.cmp.js'
+import emailSidebar from '../cmps/email-sidebar.cmp.js'
 
 export default {
   template: `
     <section class="email-app">
         <div class="layout-container">
+          
+          <div class="emails-side-bar">
+            <email-sidebar />
+          </div>
             
-            <div class="emails-container">
-              <!-- ADD: emails-filter cmp -->
-              <email-list :emails="emails" />
-            </div>
+          <div class="emails-container">
+            <email-list :emails="emails" />
+          </div>
             
-            <div class="emails-side-bar">
-              <!-- ADD: emails-folders cmp -->
-            </div>
         </div>
     </section>
   `,
@@ -33,6 +34,7 @@ export default {
 
   components: {
     emailList,
+    emailSidebar,
   },
 
   unmounted() {},
