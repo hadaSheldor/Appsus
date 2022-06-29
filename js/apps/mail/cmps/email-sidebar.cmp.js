716c75ce1be9c93a155bb5b27a56dfd34a290e39
Hadashm <hadashm@gmail.com>
2022-06-29 21:34:45 +0300

    font family and email sidebar

--- js/apps/mail/cmps/email-sidebar.cmp.js
+++ js/apps/mail/cmps/email-sidebar.cmp.js
@@ -1,23 +1,23 @@
 export default {
   template: `
     <section class="email-sidebar-container">
-        <ul>
-            <li class="inbox">inbox
+        <div>
+            <div class="inbox">inbox
                 <img src="../../../img/mail_draft_black_20dp.png" alt="">
-            </li>
-            <li class="inbox">Sent
+            </div>
+            <div class="inbox">Sent
                 <img src="../../../img/mail_send_black_20dp.png" alt="">
-            </li>
-            <li class="inbox">Bin
+            </div>
+            <div class="inbox">Bin
                 <img src="../../../img/mail_delete_black_20dp.png" alt="">
-            </li>
-            <li class="inbox">Starred
+            </div>
+            <div class="inbox">Starred
                 <img src="../../../img/mail_star_black_20dp.png" alt="">
-            </li>
-            <li class="inbox">Draft
+            </div>
+            <div class="inbox">Draft
                 <img src="../../../img/mail_draft_black_20dp.png" alt="">
-            </li>
-        </ul>
+            </div>
+        </div>
 
     </section>
     
