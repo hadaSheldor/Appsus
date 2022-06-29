0fa66e3cee68150506242af6e0df0ede7a6f1f02
Maayan <maayan.loutset@gmail.com>
2022-06-29 20:41:07 +0300

    Add new features

---
+++ js/apps/notes/pages/note-page.cmp.js
@@ -0,0 +1,17 @@
+import notesMainContainer from "../cmps/notes-main-container.cmp.js"
+
+export default {
+    template: `
+            <notes-main-container />
+    `,
+    components:{
+        notesMainContainer
+    },
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {},
+    unmounted() { },
+};
--- js/router.js
+++ js/router.js
@@ -1,7 +1,7 @@
 import homePage from './pages/home-page.cmp.js'
 import aboutPage from './pages/about-page.cmp.js'
 import mailApp from './apps/mail/pages/mail-app.js'
-import notesApp from './apps/notes/pages/note-pages.cmp.js'
+import notesApp from './apps/notes/pages/note-page.cmp.js'
 
 const routes = [
   {
