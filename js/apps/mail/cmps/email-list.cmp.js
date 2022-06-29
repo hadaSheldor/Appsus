50d334fcadb0074daeed24fbc1d72bb78e1fc5b4
Hadashm <hadashm@gmail.com>
2022-06-29 18:16:40 +0300

    format emails-date and connecting list to preview

--- js/apps/mail/cmps/email-list.cmp.js
+++ js/apps/mail/cmps/email-list.cmp.js
@@ -3,13 +3,23 @@ import emailPreview from './email-preview.cmp.js'
 export default {
   template: `
   <section v-for="email in emails" :key="email.id" class="email-list-container">
-    <email-preview :email="email"/>
+    <email-preview :email="email" @marked="onMarked" @toggleRead="onToggleRead" @remove="onRemoved" />
   </section>
 `,
 
   props: ['emails'],
 
-  methods: {},
+  methods: {
+    onMarked() {
+      console.log('marked')
+    },
+    onRemoved() {
+      console.log('removed')
+    },
+    onToggleRead() {
+      console.log('toggleRead')
+    },
+  },
 
   components: {
     emailPreview,
--- js/apps/mail/cmps/email-preview.cmp.js
+++ js/apps/mail/cmps/email-preview.cmp.js
@@ -1,13 +1,19 @@
 export default {
   template: `
     <section class="email-preview-container">
-        <div class=star>★</div>    
+        <div @click.stop="onMark(email)" class=star>★</div>    
         <span class="from">{{ email.from }}</span>
         <div class="email-preview-content">
             <div class="subject">{{ email.subject }}</div>
             <div class="body">{{ email.body }}</div>
         </div>
-        <span class="date">{{ email.sentAt }}</span>
+        <span class="date">{{ formatDate(email) }}</span>
+        <span class="trash">
+            <img @click.stop="onRemove(email.id)" src="../../../img/trash-can50 (1).png" alt="">    
+        </span>
+        <span class="read">
+            <img @click.stop="onToggleRead(email)" src="../../../img/trash-can50 (1).png" alt="">    
+        </span>
     </section>
     `,
 
@@ -19,7 +25,34 @@ export default {
 
   created() {},
 
-  methods: {},
+  methods: {
+    onRemove(id) {
+      this.$emit('remove', id)
+    },
+
+    onMark(email) {
+      this.$emit('marked', email)
+    },
+
+    onToggleRead(email) {
+      this.$emit('toggleRead', email)
+    },
+
+    formatDate(email) {
+      const longEnUSFormatter = new Intl.DateTimeFormat('en-US', {
+        month: 'long',
+        day: 'numeric',
+      })
+      const date = new Date(email.sentAt)
+      const now = Date.now()
+      const diff = now - email.sentAt
+      const day = 1000 * 60 * 60 * 24
+
+      if (diff < day) return 'Today'
+      if (diff < day * 2) return 'Yesterday'
+      return longEnUSFormatter.format(date)
+    },
+  },
 
   computed: {},
 
