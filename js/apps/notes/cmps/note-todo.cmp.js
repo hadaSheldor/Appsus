42a37577dad2c13a9709567832ab0b0db0a97002
Maayan <maayan.loutset@gmail.com>
2022-07-02 22:04:03 +0300

    Add TODO

--- js/apps/notes/cmps/note-todo.cmp.js
+++ js/apps/notes/cmps/note-todo.cmp.js
@@ -6,6 +6,9 @@ export default {
          <div v-if="note" class="note todo" :style="{ 'background-color': calcStyle }">
              <h4>TODO List: </h4><span>{{ note.info.title }}</span>
              <div><p>{{ note.info.text }}</p></div>
+             <ol>
+                <li v-for="item in note.info.todo" :style="{ 'text-decoration-line': calcItemStyle(item)}">{{ item.text}}</li>
+             </ol>
              <note-buttons-panel :note = "note" @onButtonClicked="action" />
          </div>
    `,
@@ -22,6 +25,9 @@ export default {
     methods: {
         action(action, args){
             this.$emit('noteClicked', action, this.note, args)
+        },
+        calcItemStyle(item){
+            return item.isChecked ? "line-through" : "none";
         }
     },
     computed: {
