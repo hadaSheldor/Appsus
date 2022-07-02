17a979fdd72848d2b2e65d60aaa2d27953ecca08
Maayan <maayan.loutset@gmail.com>
2022-07-02 21:37:53 +0300

    Update main menu behaviour

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -5,16 +5,24 @@ export default {
            <img class="logo" src='../../img/icons-gmail-48.PNG'>
        </div> -->
        <nav class="nav-bar">
-         <router-link class="main-menu-item" to="/">Home</router-link>
-         <router-link class="main-menu-item"  to="/email/inbox">eMail</router-link>
-         <router-link class="main-menu-item"  to="/notes">Notes</router-link>
-         <router-link class="main-menu-item" to="/about">About</router-link>
+         <router-link class="main-menu-item" active-link="active" @click="setActive($event)" to="/">Home</router-link>
+         <router-link class="main-menu-item" active-link="active" @click="setActive($event)" to="/email/inbox">eMail</router-link>
+         <router-link class="main-menu-item" active-link="active" @click="setActive($event)" to="/notes">Notes</router-link>
+         <router-link class="main-menu-item" active-link="active"  @click="setActive($event)" to="/about">About</router-link>
        </nav>
     </header>
    `,
   data() {
     return {}
   },
-  methods: {},
+  methods: {
+    setActive(event){
+    //   const menuItems = this.$el.querySelectorAll(".main-menu-item")
+    //   menuItems.forEach(element => {
+    //     element.classList.remove('active')
+    //   })
+    //   event.target.classList.add('active')
+     }
+  },
   computed: {},
 }
